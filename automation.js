72fea63d87d12c5b21485a228798a1e514891571
Lyainc <koore12@gmail.com>
2023-10-27 18:48:13 +0900

    슬랙봇 초기설정

--- automation.js
+++ automation.js
@@ -8,8 +8,18 @@ function onFormSubmit(e) {
     // Get the values from the new row
     var values = sheet.getRange(row, 1, 1, sheet.getLastColumn()).getValues()[0];
     
-    // Do something with the values
-    // For example, log the values to the console
-    console.log(values);
+    // Set up the Slack webhook URL and message
+    var webhookUrl = "https://hooks.slack.com/services/YOUR/SLACK/WEBHOOK/URL";
+    var message = "New row added:\n" + values.join("\n");
+    
+    // Send the message to Slack using the webhook
+    var payload = {
+      "text": message
+    };
+    var options = {
+      "method": "POST",
+      "payload": JSON.stringify(payload)
+    };
+    UrlFetchApp.fetch(webhookUrl, options);
   }
   
