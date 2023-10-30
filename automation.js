bf7c69c622f72fb18abfb6e435168a66dff9eae0
Lyainc <koore12@gmail.com>
2023-10-30 18:55:27 +0900

    전체 리팩토링

--- automation.js
+++ automation.js
@@ -8,18 +8,22 @@ function sendMessage(e) {
   // Get the values from the new row
   var values = sheet.getRange(row, 1, 1, sheet.getLastColumn()).getValues()[0];
   
-  // Set up the Slack webhook URL and message
+  // Set up the Slack webhook URL and timestamp
   var webhookUrl = "https://hooks.slack.com/services/YOUR/SLACK/WEBHOOK/URL";
   var timestamp = values[0];
   var formattedDate = Utilities.formatDate(new Date(timestamp), 'Asia/Seoul', 'yyyy-MM-dd');
 
   // Define the message
+  var message = "*[신규 입사자 알림]*\n\n" +
+                "-----------------------------\n\n" +
+                "- 제출 일자: " + formattedDate + "\n" +
+                "- 이름: " + values[1] + "\n" + // Assuming the name is in the second column
+                // Add more data as needed
+                "...\n";
+
+  // Create the payload for the Slack message
   var payload = {
-    "text" : "*[신규 입사자 알림]*" + "\n" + "\n" +
-    "-----------------------------" + "\n" + "\n" +
-    "- 제출 일자" + formattedDate + "\n" +
-    "- 이름: " + data[1] + "\n" +
-    "..."
+    "text": message
   };
 
   // Configure the options for the HTTP request to Slack
